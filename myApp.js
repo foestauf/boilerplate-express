bf76855c9a8c9f3db49c24e8319c7765bb17ee88
foestauf <46908845+foestauf@users.noreply.github.com>
2020-01-22 19:34:38 -0500

    Complete of NODEJS module

--- myApp.js
+++ myApp.js
@@ -1,12 +1,24 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
 
 
-app.use(express.static(__dirname + "/public"));
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+app.use(function (req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip)
+    next()
+});
 
+// app.get("/", function(req, res) {
+//    res.sendFile(__dirname + '/views/index.html');
+// });
 
-console.log("Hello World");
+// app.use(express.static(__dirname + "/public"));
+
 // --> 7)  Mount the Logger middleware here
 
 
@@ -20,9 +32,6 @@ console.log("Hello World");
 
 
 /** 3) Serve an HTML file */
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-});
 
 
 /** 4) Serve static assets  */
@@ -35,31 +44,49 @@ app.get("/json", (req, res) => {
     });
 });
 
-
 /** 6) Use the .env file to configure the app */
-const response = ()=>{
+const response = () => {
     let resp = " "
-    if (process.env.MESSAGE_STYLE === 'uppercase'){
+    if (process.env.MESSAGE_STYLE === "uppercase") {
         return resp = "Hello json".toUpperCase();
     } else {
         return resp = "Hello json";
     }
 }
- 
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
+
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+},
+    (req, res) => {
+        res.send({
+            time: req.time
+        });
+    }
+);
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", (req, res) => {
+    res.json({
+        echo: req.params.word,
+    })
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
+app.route("/name").get((req, res) => {
+    var first = req.query.first;
+    var last = req.query.last;
+    var jsonObj = { name: first + " " + last };
+    res.json(jsonObj);
+}).post(urlencodedParser, (req, res) => {
+    res.json({ "name": req.body.first + ' ' + req.body.last });
+});
 
-  
+
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
@@ -74,4 +101,4 @@ const response = ()=>{
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;
